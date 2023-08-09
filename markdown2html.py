3d3308dea21f2407a8c95df5d61bb59cd28e56cf
YeosCRN <jeoscrn20@outlook.com>
2023-08-09 15:40:09 -0500

    md2html4.0

--- markdown2html.py
+++ markdown2html.py
@@ -5,15 +5,19 @@
 - Second argument is the output file name, makes no change'''
 import sys
 
-# takes an argument of 2 strings
-x = len(sys.argv)
+def main(*args):
+    # takes an argument of 2 strings
+    x = len(sys.argv)
 
-if x <= 2:
-    print("Usage: ./markdown2html.py README.md README.html", file=sys.stderr)
-    exit (1)
-try:
-    file = open(sys.argv[1])
-    file.close()
-except:
-    print("Missing ", sys.argv[1])
-    exit(1)
+    if x <= 2:
+        print("Usage: ./markdown2html.py README.md README.html", file=sys.stderr)
+        exit (1)
+    try:
+        file = open(sys.argv[1])
+        file.close()
+    except:
+        print("Missing ", sys.argv[1])
+        exit(1)
+
+if __name__=="__main__":
+    main(sys.argv)
