90cf844b243e1b8710cb17588c6dccf24797b366
YeosCRN <jeoscrn20@outlook.com>
2023-08-10 10:33:39 -0500

    headings2.0

--- markdown2html.py
+++ markdown2html.py
@@ -1,5 +1,4 @@
 #!/usr/bin/python3
-
 '''A script markdown2html.py that takes an argument of 2 strings:
 - First argument is the name of the Markdown file
 - Second argument is the output file name, makes no change'''
@@ -12,17 +11,17 @@ def main(*args):
 
     if x <= 2:
         print("Usage: ./markdown2html.py README.md README.html", file=sys.stderr)
-        exit (1)
+        exit(1)
     try:
         file = open(sys.argv[1], 'r')
         og_text = file.read()
         copy_text = markdown.markdown(og_text)
         with open(sys.argv[2], 'w') as f:
             f.write(copy_text)
-        return True
     except:
         print("Missing", sys.argv[1], file=sys.stderr)
         exit(1)
 
-if __name__=="__main__":
+
+if __name__ == "__main__":
     main(sys.argv)
