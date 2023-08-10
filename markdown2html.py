a60333a33e21ca546ffb7bfeeaf907bafe223e07
YeosCRN <jeoscrn20@outlook.com>
2023-08-10 14:41:24 -0500

    Test

--- markdown2html.py
+++ markdown2html.py
@@ -5,10 +5,14 @@
 """- First argument is the name of the Markdown file
 - Second argument is the output file name, makes no change"""
 
-import sys
-import markdown
 
 # Function to convert Markdown to HTML
+
+
+
+
+import sys
+import markdown
 def main(*args):
     """Take an argument of 2 strings."""
     x = len(sys.argv)
@@ -20,8 +24,10 @@ def main(*args):
         # Read the content of the Markdown file
         file = open(sys.argv[1], 'r')
         og_text = file.read()
+        og_text = preprocess_markdown(og_text)
         # Convert Markdown text to HTML
-        copy_text = markdown.markdown(og_text)
+        copy_text = markdown.markdown(
+            og_text, extensions=['markdown.extensions.extra'])
         file.close()
         # Write the HTML content to the output file
         with open(sys.argv[2], 'w') as f:
@@ -30,6 +36,16 @@ def main(*args):
     except:
         print("Missing", sys.argv[1], file=sys.stderr)
         exit(1)
+
+
+def preprocess_markdown(og_text):
+    lines = og_text.split("\n")
+    preprocessed_lines = []
+    for line in lines:
+        if line.strip().startswith("*"):
+            line = "1. " + line.strip("*").strip()
+        preprocessed_lines.append(line)
+    return "\n".join(preprocessed_lines)
 
 
 if __name__ == "__main__":
